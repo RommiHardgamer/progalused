8936bcefe608e95e95b192ec686218ccba64ad5f
rommiHardgamer <romet.mottus@gmail.com>
2024-11-20 15:33:44 +0200

    funktsiooni ülesanne

---
+++ funktsioon.py
@@ -0,0 +1,26 @@
+"""Function examples."""
+
+
+def func():
+    print("I´m inside the function")
+
+def my_name_is(name):
+    print(f"My name is {name}") 
+
+def sum_six(num):
+# sum_six(num)
+    return 6 + num
+
+def sum_numbers(a,b)
+Saada tulemused E-mailile
+
+Esitused
+2024-10-24 13:49:
+    return a + b
+    print (sum_numbers(8,9))
+# sum_numbers()
+
+def usd_to_eur(usd):
+    return usd*0.8
+print(usd_to_eur(1)) 
+# usd_to_eur()
